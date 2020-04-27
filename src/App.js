f97b59674e78a0f755de3c3a439a9c7d57737431
liyj <liyj@bwoil.com>
2020-04-27 15:54:36 +0800

    引入eslint

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <h2>React Hooks</h2>
+          <h3>React Mixin HOC(high-order-component)</h3>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
