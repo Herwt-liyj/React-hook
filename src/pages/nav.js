d756e9f2069ab44c21b0f08a6bbcd15b68a12c0b
liyj <liyj@bwoil.com>
2020-04-27 18:56:40 +0800

    navlist 导航

---
+++ src/pages/nav.js
@@ -0,0 +1,13 @@
+class NavList extends React.Component {
+    render() {
+        <div class='nav-list'>
+            <h1>Demo React Hook Nav list for {this.props.name} </h1>
+            <ul>
+                <li>Form表单</li>
+                <li>Router路由</li>
+            </ul>
+        </div>
+    }
+}
+
+export default NavList
